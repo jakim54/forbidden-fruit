d43366b906824159978885d3a4ca2422cdf82e63
jajuki13 <khkim1776@gmail.com>
2018-01-16 03:50:02 -0500

    comment and variable

--- codeSnippets/helloWorld.py
+++ codeSnippets/helloWorld.py
@@ -6,3 +6,7 @@ print()
 print("escape punctuations in git commit messages")
 print("Find REPL for Python3 in Sublime3")
 print("Parenthesis" + "ConCat")
+
+question = "Is this a variable?"
+# this is a comment. cmd + ?
+print(question)
