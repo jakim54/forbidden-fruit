c72f0ca6e1986a744d1f2164feb53b29b793b1d5
jajuki13 <khkim1776@gmail.com>
2018-01-16 04:10:03 -0500

    fixed parenthesis issue

--- codeSnippets/helloWorld.py
+++ codeSnippets/helloWorld.py
@@ -10,3 +10,12 @@ print("Parenthesis" + "ConCat")
 question = "Is this a variable?"
 # this is a comment. cmd + ?
 print(question)
+
+# Classic Input/Output Demonstration
+
+greeting = "Hello"
+name = input("Please enter your name")
+print(greeting + ' ' + name)
+
+splitString = "This string has been\nsplit over\nseveral\lines"
+print("Check if this fixed the JEDI autocomplete issues regarding the left parenthesis")
