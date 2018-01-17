0454650d1065953f5b1597eab7930f7708ddfecf
jajuki13 <khkim1776@gmail.com>
2018-01-16 23:24:49 -0500

    string formatting stuff

---
+++ codeSnippets/stringFormatting.py
@@ -0,0 +1,13 @@
+age = 24
+
+# str method to convert variable age from int
+# single target
+print("My age is " + str(age) + " years")
+
+# method format replaces matching index items
+print("My age is {0} years.".format(age))
+# better example
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, and {7}.".format(31, "January", "March", "May", "July", "August", "October", "December"))
+
+# using triple quote to format nicely
+print("January", "March", "May", "July", "August", "October", "December"))
