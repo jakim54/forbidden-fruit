e37b6d09b320d232dc50edadbaaae05804c42581
jajuki13 <khkim1776@gmail.com>
2018-01-18 23:47:51 -0500

    added flow control, challenge 1

---
+++ codeSnippets/challengeNum1.py
@@ -0,0 +1,4 @@
+# Write a small program to ask for a name an age.
+# When both values have been entered, check if the person is the right age to go on an 18 ~ 30 holiday.
+# They must be over 18, but under 31.
+
