42ea93082e7271a7ca8c529691fa4503cf729bf7
jajuki13 <khkim1776@gmail.com>
2018-01-18 22:14:52 -0500

    Finished string formatting

--- codeSnippets/stringFormatting.py
+++ codeSnippets/stringFormatting.py
@@ -6,6 +6,9 @@ print("My age is " + str(age) + " years")
 
 # method format replaces matching index items
 print("My age is {0} years.".format(age))
+
+print("")
+
 # better example
 print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6}, and {7}.".format(31, "January", "March", "May", "July", "August", "October", "December"))
 
@@ -23,13 +26,35 @@ print("""January: {2}
     November: {1}
     December: {2}""".format(28, 30, 31))
 
+print("")
+
 # String Formatting Operator deprecated now for Python 3?
 # Similar to Obj-C's.
+# {#} replacement field being better than % due to matching than ordered
 print("My age is %d years" % age)
 print("My age is %d %s, %d %s" % (age, "years", 6, "months"))
 
 print("")
 
 # Taste of for loop within the print statement
+# The number 2 and 4 in between % and d is for formatting on console
 for i in range(1, 12):
-    print("No. #2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
+    print("No. %2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
+
+print("")
+print("Pi is approximately %12.50f" % (22 / 7))
+print("")
+
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:4} and cubed is {2:4}". format(i, i ** 2, i ** 3))
+
+print("")
+
+# formatting the output on the console to be left aligned:
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:<4} and cubed is {2:<4}". format(i, i ** 2, i ** 3))
+
+print("")
+
+# formatted differently
+print("Pi is approximately {0:12.50f}".format(22 / 7))
